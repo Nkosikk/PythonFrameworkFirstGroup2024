9b6bb1edb2fab80f62b1cd45dab31f3e0015f095
Cele <njceles@gmail.com>
2024-04-09 19:02:39 +0200

    created login test class

---
+++ utils/test_login.py
@@ -0,0 +1,22 @@
+import allure
+import pytest
+
+from pageObjects.loginPage import LoginPage
+from utils.readProperties import ReadConfig
+
+
+class test_001_LoginToSauceDemo:
+    BaseUrl = ReadConfig().getBaseURL()
+    Username = ReadConfig().getUsername()
+    Password = ReadConfig().getPassword()
+
+    @pytest.mark.Regression
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_loginTests(self, setup):
+        self.driver = setup
+        self.driver.get(self.BaseUrl)
+        self.driver.maximize_window()
+        self.lp = LoginPage(self.driver)
+        self.lp.enterUsername(self.Username)
+        self.lp.enterPassword(self.Password)
+        self.lp.clickLoginButton()
